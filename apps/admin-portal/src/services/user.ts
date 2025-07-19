09733fa3dd324f581ec0d7ad645642a8cacf37a8
Frankj9052 <jiajuronghot@hotmail.com>
2025-07-19 17:52:09 -0400

    jurong-adminPortal-user-finish

--- apps/admin-portal/src/services/user.ts
+++ apps/admin-portal/src/services/user.ts
@@ -1,5 +1,4 @@
-import { get } from '@/libs/axios/client';
-import { ActionResult } from '@/types';
+import { del, get, patch } from '@/libs/axios/client';
 import {
   ApiResponse,
   idParamsSchema,
@@ -7,10 +6,11 @@ import {
   userListRequestSchema,
   UserListResponse,
   UserSingleResponse,
+  UserUpdateRequest,
+  userUpdateRequestSchema,
 } from '@frankjhub/shared-schema';
-import axios from 'axios';
-
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+import { ValidationError } from '@frankjhub/shared-errors';
+import { convertZodIssuesToErrorDetails } from '@frankjhub/shared-utils';
 
 export async function getUserProfileClient(): Promise<ApiResponse<UserSingleResponse>> {
   const response = await get<UserSingleResponse>('/api/user/current-user-profile');
@@ -21,134 +21,74 @@ export async function getUsersAllProfile(
   pagination: UserListRequest
 ): Promise<ApiResponse<UserListResponse>> {
   const parsedInput = userListRequestSchema.safeParse(pagination);
+  if (!parsedInput.success) {
+    const error = new ValidationError(convertZodIssuesToErrorDetails(parsedInput.error)).toJSON();
+    return error;
+  }
   const response = await get<UserListResponse>(`/api/user/list`, {
     params: parsedInput,
   });
-  if (!parsedInput.success) {
-    return {
-      status: 400,
-      code: 'VALIDATION_ERROR',
-      message: 'Invalid get user list request',
-      details: parsedInput.error.issues,
-      timestamp: new Date().toISOString(),
-    };
-  }
-  try {
-    const parsed = userAllProfilePaginationSchema.parse(pagination);
-
-    const res = await axios.get(`${baseURL}/api/user/users-all-profile`, {
-      withCredentials: true,
-      params: parsed,
-    });
-    return { status: 'success', data: res.data };
-  } catch (err) {
-    console.log(err);
-    const message = axios.isAxiosError(err)
-      ? err.response?.data?.details || err.message
-      : 'Unknown get users error';
-    return { status: 'error', error: message };
-  }
+  return response;
 }
 
 export async function getUserAllProfileById({
   id,
 }: {
   id: string;
-}): Promise<ActionResult<UserAllProfileResponse>> {
-  try {
-    const parsed = idParamsSchema.parse({ id });
-    const res = await axios.get(`${baseURL}/api/user/${parsed.id}`, {
-      withCredentials: true,
-    });
-    return { status: 'success', data: res.data };
-  } catch (err) {
-    console.log(err);
-    const message = axios.isAxiosError(err)
-      ? err.response?.data?.details || err.message
-      : 'Unknown get user error';
-    return { status: 'error', error: message };
+}): Promise<ApiResponse<UserSingleResponse>> {
+  const parsedInput = idParamsSchema.safeParse({ id });
+  if (!parsedInput.success) {
+    const error = new ValidationError(convertZodIssuesToErrorDetails(parsedInput.error)).toJSON();
+    return error;
   }
+  const response = await get<UserSingleResponse>(`/api/user/${parsedInput.data.id}`);
+  return response;
 }
 
-export async function softDeleteUser(id: string): Promise<ActionResult<string>> {
-  try {
-    const parsed = idParamsSchema.parse({ id });
-    await axios.patch(
-      `${baseURL}/api/user/soft-delete`,
-      {
-        id: parsed.id,
-      },
-      {
-        withCredentials: true,
-      }
-    );
-    return { status: 'success', data: 'Delete successfully' };
-  } catch (err) {
-    console.log(err);
-    const message = axios.isAxiosError(err)
-      ? err.response?.data?.details || err.message
-      : 'Unknown get user error';
-    return { status: 'error', error: message };
+export async function softDeleteUser(id: string): Promise<ApiResponse<UserSingleResponse>> {
+  const parsedInput = idParamsSchema.safeParse({ id });
+  if (!parsedInput.success) {
+    const error = new ValidationError(convertZodIssuesToErrorDetails(parsedInput.error)).toJSON();
+    return error;
   }
+  const response = await patch<UserSingleResponse>(`/api/user/soft-delete`, { id: parsedInput });
+  return response;
 }
 
-export async function restoreDeletedUser(id: string): Promise<ActionResult<string>> {
-  try {
-    const parsed = idParamsSchema.parse({ id });
-    await axios.patch(
-      `${baseURL}/api/user/restore`,
-      {
-        id: parsed.id,
-      },
-      {
-        withCredentials: true,
-      }
-    );
-    return { status: 'success', data: 'User restored successfully' };
-  } catch (err) {
-    console.log(err);
-    const message = axios.isAxiosError(err)
-      ? err.response?.data?.details || err.message
-      : 'Unknown get user error';
-    return { status: 'error', error: message };
+export async function restoreDeletedUser(id: string): Promise<ApiResponse<UserSingleResponse>> {
+  const parsedInput = idParamsSchema.safeParse({ id });
+  if (!parsedInput.success) {
+    const error = new ValidationError(convertZodIssuesToErrorDetails(parsedInput.error)).toJSON();
+    return error;
   }
+  const response = await patch<UserSingleResponse>(`/api/user/restore`, { id: parsedInput });
+  return response;
 }
 
-export async function hardDeleteUser(id: string): Promise<ActionResult<string>> {
-  try {
-    const parsed = idParamsSchema.parse({ id });
-    await axios.delete(`${baseURL}/api/user/hard-delete`, {
-      withCredentials: true,
-      params: {
-        id: parsed.id,
-      },
-    });
-    return { status: 'success', data: 'User deleted permanently!' };
-  } catch (err) {
-    console.log(err);
-    const message = axios.isAxiosError(err)
-      ? err.response?.data?.details || err.message
-      : 'Unknown get user error';
-    return { status: 'error', error: message };
+export async function hardDeleteUser(id: string): Promise<ApiResponse<UserSingleResponse>> {
+  const parsedInput = idParamsSchema.safeParse({ id });
+  if (!parsedInput.success) {
+    const error = new ValidationError(convertZodIssuesToErrorDetails(parsedInput.error)).toJSON();
+    return error;
   }
+  const response = await del<UserSingleResponse>(`/api/user/hard-delete`, {
+    params: {
+      id: parsedInput.data.id,
+    },
+  });
+  return response;
 }
 
-export async function adminUpdateUser(data: UserAdminUpdateSchema): Promise<ActionResult<string>> {
-  try {
-    const parsed = userAdminUpdateSchema.parse(data);
-    await axios.patch(
-      `${baseURL}/api/user/admin-update`,
-      { ...parsed },
-      {
-        withCredentials: true,
-      }
-    );
-    return { status: 'success', data: 'User data updated!' };
-  } catch (err) {
-    console.log(err);
-    const message = axios.isAxiosError(err)
-      ? err.response?.data?.details || err.message
-      : 'Unknown admin update user error';
-    return { status: 'error', error: message };
+export async function adminUpdateUser(
+  data: UserUpdateRequest
+): Promise<ApiResponse<UserSingleResponse>> {
+  const parsedInput = userUpdateRequestSchema.safeParse(data);
+  if (!parsedInput.success) {
+    const error = new ValidationError(convertZodIssuesToErrorDetails(parsedInput.error)).toJSON();
+    return error;
   }
+  const response = await patch<UserSingleResponse>('/api/user/admin-update', {
+    ...parsedInput.data,
+  });
+  return response;
 }
