22291ec9416fd51cc617a268a09b45d5046d5df7
Frankj9052 <jiajuronghot@hotmail.com>
2025-09-25 10:47:43 -0400

    jurong-patient-clinic-portal-created-draft

---
+++ apps/patient-portal/next.config.js
@@ -0,0 +1,35 @@
+//@ts-check
+
+const { composePlugins, withNx } = require('@nx/next');
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  // Use this to set Nx-specific options
+  // See: https://nx.dev/recipes/next/next-config-setup
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        pathname: '/gh/faker-js/assets-person-portrait/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  nx: {
+    svgr: false,
+  },
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx,
+];
+
+module.exports = composePlugins(...plugins)(nextConfig);
